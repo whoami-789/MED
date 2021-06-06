fe85d73cf91bcfc3503e789baf58f75e9e550e4e
whoami-789 <tudya1238@gmail.com>
2021-06-07 01:51:29 +0300

    git commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -45,4 +45,11 @@ public class Controller {
     public void del(){
 
     }
+    public void back(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Main.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
